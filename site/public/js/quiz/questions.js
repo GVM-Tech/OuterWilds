55c8755f05f57614353ca5763e6f5aa1a06ebe4b
ViniciusFernandes09 <contato.viniciusfernandes9@gmail.com>
2024-07-12 21:27:48 -0300

    Perguntas do quiz

---
+++ site/public/js/quiz/questions.js
@@ -0,0 +1,95 @@
+export default [
+    {
+      question: "Qual foi o ano de lançamento de Outer Wilds?",
+      answers: [
+        { option: "A) 2022", correct: false },
+        { option: "B) 2016", correct: false },
+        { option: "C) 2019", correct: true },
+        { option: "D) 2020", correct: false },
+      ],
+    },
+
+    {
+      question: "Riebeck toca violão?",
+      answers: [
+        { option: "A) Verdadeiro", correct: false },
+        { option: "B) Falso", correct: true },
+      ],
+    },
+
+    {
+      question: "Quantos finais existem?",
+      answers: [
+        { option: "A) 10", correct: false },
+        { option: "B) 1", correct: false },
+        { option: "C) 5", correct: false },
+        { option: "D) 6", correct: true },
+      ],
+    },
+
+    {
+      question: "Quando os nomai receberam o sinal do olho, eles usaram uma nave avanca para se teleportar para perto do sinal, qual o nome dessa nave?",
+      answers: [
+        { option: "A) Hospedeiro", correct: true },
+        { option: "B) Navegador", correct: false },
+        { option: "C) Nave de translocação", correct: false },
+        { option: "D) Exploradora", correct: false },
+      ],
+    },
+
+    {
+      question: "Recanto lenhoso tem uma lua vulcanica?",
+      answers: [
+        { option: "A) Verdadeiro", correct: false },
+        { option: "B) Falso", correct: true },
+      ],
+    },
+
+    {
+      question: "Quantos planetas existem no universo de Outer Wilds?",
+      answers: [
+        { option: "A) 2", correct: false },
+        { option: "B) 4", correct: false },
+        { option: "C) 6", correct: true },
+        { option: "D) 9", correct: false },
+      ],
+    },
+
+    {
+      question: "Gabro esta acampado em qual planeta?",
+      answers: [
+        { option: "A) Vale Incerto", correct: false },
+        { option: "B) Profundezas do Gigante", correct: true },
+        { option: "C) Abrolho Sombrio", correct: false },
+        { option: "D) Recanto Lenhoso", correct: false },
+      ],
+    },
+
+    {
+      question: "O jogo foi lançado em todas as plataformas?",
+      answers: [
+        { option: "A) Verdadeiro", correct: true },
+        { option: "B) Falso", correct: false },
+      ],
+    },
+
+    {
+      question: "Qual o nome da estrutura que fica na órbita do sol?",
+      answers: [
+        { option: "A) Sonda de orbita solar", correct: false },
+        { option: "B) Estação solar", correct: true },
+        { option: "C) Anel de Saturno", correct: false },
+        { option: "D) Estação estelar", correct: false },
+      ],
+    },
+
+    {
+      question: "Qual o nome do protagonista?",
+      answers: [
+        { option: "A) Hearthian", correct: false },
+        { option: "B) Não é revelado", correct: true },
+        { option: "C) Esker", correct: false },
+        { option: "D) Feldspato", correct: false },
+      ],
+    },
+  ];

f56a378216ec6c05b346d573189a0034f2ed8341
ViniciusFernandes09 <contato.viniciusfernandes9@gmail.com>
2024-07-17 23:47:07 -0300

    Grafico do quiz 1.2

--- site/app.js
+++ site/app.js
@@ -1,11 +1,22 @@
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
+// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+
+require("dotenv").config({ path: caminho_env });
+
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
+var PORTA_APP = 3333;
+var HOST_APP = 'localhost';
 
 var app = express();
 
 var indexRouter = require("./src/routes/index");
+var usuarioRouter = require("./src/routes/usuarios");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,14 +25,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
+app.use("/usuarios", usuarioRouter);
 
-app.listen(PORTA, function () {
-        console.log(`
-——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
-\t\t\t\t\tO servidor GVM está online!
-\t\t\tAcesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
-——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n
-\t\t\tVocê está utilizando o banco de dados local(MySQL Workbench).\n
-——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————\n`
-        );
-    });
+app.listen(PORTA_APP, function () {
+    console.log(`
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    \n\n\n                                                                                                 
+    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
+    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
+    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
+    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+});
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -1,37 +1,28 @@
-var database = require("../database/config");
+var database = require("../database/config")
 
-function listar() {
-  var instrucao = `select email from usuario;`;
-  return database.executar(instrucao);
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
-function entrar(email, senha) {
-  var instrucao = ''
-  if (process.env.AMBIENTE_PROCESSO == "producao") {
-    instrucao = `SELECT 
-    (SELECT SUBSTRING(nome, 1, CHARINDEX(' ', nome + ' ') - 1) AS primeiro_nome FROM usuario WHERE email = '${email}' AND senha = (HASHBYTES('SHA2_256','${senha}'))) as nome,nome as nomeCompleto, idUsuario, email, cpf, senha, cargo, fkEmpresa FROM usuario WHERE email = '${email}' AND senha = (HASHBYTES('SHA2_256','${senha}'));`;
-  } else {
-    instrucao = `SELECT * FROM usuario WHERE email = '${email}' AND senha = sha2('${senha}', 256);`;
-  }
-  return database.executar(instrucao);
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha, empresaId) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${empresaId}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
-
-function cadastrar(nome, email, cpf, senha) {
-  var instrucao = ''
-  if (process.env.AMBIENTE_PROCESSO == "producao") {
-    instrucao = `insert into [dbo].[usuario] (nome, email, cpf, senha, cargo) values ('${nome}','${email}','${cpf}',HASHBYTES('SHA2_256','${senha}'),'Dono');`;
-  } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-    instrucao = `INSERT INTO usuario (nome, email, cpf, senha, cargo ) VALUES ('${nome}', '${email}','${cpf}',sha2('${senha}', 256),'Dono');`;
-  } else {
-    return
-  }
-  return database.executar(instrucao);
-}
-
-
 
 module.exports = {
-  cadastrar,
-  entrar,
-  listar,
+    autenticar,
+    cadastrar
 };
